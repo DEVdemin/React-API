cbd842681190ff1f0b8990a8d37ec2055665a69f
DaniloKunzendorff <danilo.k.oliveirat@gmail.com>
2022-05-25 20:53:56 -0300

    a

--- src/pages/media/index.js
+++ src/pages/media/index.js
@@ -1,22 +1,44 @@
 import './index.scss';
-
+import { useState } from "react";
+import axios from "axios"
 
 export default function Index(){
-    //const [Numero, setNumero] = useState(0);
-    
+    const [nota1, setNota1] = useState();
+    const [nota2, setNota2] = useState();
+    const [nota3, setNota3] = useState();
+    const [resposta, setResposta]= useState('');
+
+    async function calcularMedia() {
+        const resp = await axios.post('http://localhost:5000/media', {
+            
+        })
+    }   
+
     return(
         <main> 
             <h1>Calcular Média</h1>
 
             <div> 
-                Primeira nota: <input type = "text" />
+                Primeira nota: <input type = "text" value={nota1} onChange = {e => setNota1(e.target.value) } />
             </div>
             <div> 
-                Segunda nota: <input type = "text" />
+                Segunda nota: <input type = "text" value={nota2} onChange = {e => setNota2(e.target.value) } />
             </div>
             <div> 
-                Terceira nota: <input type = "text" />
+                Terceira nota: <input type = "text" value={nota3} onChange = {e => setNota3(e.target.value) } />
             </div>
+
+            <div> 
+                <button>Calcular</button>
+            </div>
+
+            <div> 
+                A Média é: {resposta}
+            </div>
+
+            <a href="/">Voltar</a>
+
+
         </main>
     )
 
--- src/siteRoutes.js
+++ src/siteRoutes.js
@@ -6,6 +6,7 @@ import Frequencia from './pages/Frequencia'
 import Ingresso from './pages/Ingresso'
 import MaiorNumero from './pages/Maior Número'
 import Dobro from './pages/Dobro'
+import Media from './pages/media'
 
 export default function SiteRoutes(){
     return(
@@ -17,6 +18,7 @@ export default function SiteRoutes(){
                             <Route path='/ingresso' element={<Ingresso/>} />
                             <Route path='/maiorNumero' element={<MaiorNumero/>} />
                             <Route path='/dobro' element={<Dobro/>} />
+                            <Route path='/media' element={<Media/>} />
                     </Routes>
         </BrowserRouter>
     )
