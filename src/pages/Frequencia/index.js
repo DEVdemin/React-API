1e972c97d60c6b4e556e91e1e448fc6a69b1d667
DaniloKunzendorff <danilo.k.oliveirat@gmail.com>
2022-05-25 16:21:05 -0300

    freq

--- src/pages/Frequencia/index.js
+++ src/pages/Frequencia/index.js
@@ -1,7 +1,38 @@
-export default function index(){
+import { useState } from "react"
+import axios from "axios";
+
+export default function Index(){
+    const [texto, setTexto] = useState('');
+    const [caractere, setCaractere] = useState('');
+    const [resposta, setResposta] = useState('');
+
+    async function verificarFrequencia() {
+        const resp = await axios.get(`http://localhost:5000/dia2/freqcaracter/${texto}/${caractere}`)
+        setResposta(resp.data.Freq)
+    }
+
     return (
+
+
         <main> 
-            <h1> Frequêcia de um caracter</h1>
+            <h1> Frequêcia de um caractere</h1>
+
+            <div>
+                Texto: <input type='text' value={texto} onChange= {e => setTexto(e.target.value)} />
+            </div>
+
+            <div>
+                Caractere: <input type='text' value={caractere} onChange= {e => setCaractere(e.target.value)} />
+            </div>
+
+            <div>
+                <button onClick={verificarFrequencia} > Verificar Frequêcia de Caractere </button>
+            </div>
+
+            <div>
+                Frequêcia: {resposta}
+            </div>
+
         </main>
     )
 }
