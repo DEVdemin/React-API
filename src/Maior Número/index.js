9b694c5a82bbd6c17e6549f72daa3aad2370fc74
Pedro <pedroreisrodrigues2905@gmail.com>
2022-05-25 17:57:55 -0300

    Pagina de ingressos feita

---
+++ src/Maior Número/index.js
@@ -0,0 +1,7 @@
+export default function index(){
+    return (
+        <main> 
+            <h1> Maior Numero </h1>
+        </main>
+    )
+}
--- src/pages/Ingresso/index.js
+++ src/pages/Ingresso/index.js
@@ -1,7 +1,50 @@
-export default function index(){
+import { useState } from "react";
+import axios from "axios"
+
+
+
+export default function Index(){
+        const [qtdInteiras,setqtdInteiras] = useState(0);
+        const [qtdMeias,setqtdMeias] = useState(0);
+        const [diaSemana,setDiaSemana] = useState('');
+        const [Nacionalidade,setNacionalidade] = useState('');
+        const [Total, setTotal] = useState(0);
+
+       async function calcular(){
+         const resp = await axios.post('http://localhost:5000/ingresso', {
+            a:qtdInteiras,
+            b:qtdMeias,
+            c:diaSemana,
+            d:Nacionalidade
+            })
+
+            setTotal(resp.data.Total)
+        }
+
+
+        
     return (
         <main> 
             <h1> Ingresso </h1>
+
+            <div> 
+                Qtd.Inteiras:<input type = "text"  value={qtdInteiras} onChange={e=> setqtdInteiras(Number(e.target.value))}/>
+            </div>
+            <div> 
+                Qtd.Meias:<input type = "text" value={qtdMeias} onChange={e=> setqtdMeias(Number(e.target.value))}  />
+            </div>
+            <div> 
+                Dia da Semana:<input type = "text" value={diaSemana} onChange={e=> setDiaSemana(e.target.value)}/>
+            </div>
+            <div> 
+                Nacionalidade:<input type = "text" value={Nacionalidade} onChange={e=> setNacionalidade (e.target.value)} />
+            </div>
+            <div> 
+                <button onClick={calcular}> Calcular </button>
+            </div>
+            <div> 
+                O total é de: {Total}
+             </div>
         </main>
     )
 }
