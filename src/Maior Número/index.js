688d43d9864d8341e3bfd302cb43c37044ec41b5
DaniloKunzendorff <danilo.k.oliveirat@gmail.com>
2022-05-25 20:30:22 -0300

    a

--- src/Maior Número/index.js
+++
@@ -1,7 +0,0 @@
-export default function index(){
-    return (
-        <main> 
-            <h1> Maior Numero </h1>
-        </main>
-    )
-}
---
+++ src/pages/media/index.js
@@ -0,0 +1,15 @@
+import './index.scss';
+
+
+export default function Index(){
+    //const [Numero, setNumero] = useState(0);
+    
+    return(
+        <main> 
+            <h1>Calcular Média</h1>
+
+
+        </main>
+    )
+
+}
